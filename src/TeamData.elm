232b6482b964c75cdb12f2b9e67caf966ca7d426
tombavarsky <tomba16062004@gmail.com>
2020-01-31 11:06:56 +0200

    delete Team

--- src/TeamData.elm
+++ src/TeamData.elm
@@ -14,7 +14,6 @@ type Msg
     = ScouterInput String
     | MatchInput String
     | Station Stations
-    | Team
 
 
 type alias Model =
@@ -154,9 +153,6 @@ update msg model =
 
         MatchInput match ->
             { model | match = String.toInt match }
-
-        Team ->
-            { model | team = getMatch model.match <| stationToString model.station }
 
 
 subscriptions : Sub Msg
