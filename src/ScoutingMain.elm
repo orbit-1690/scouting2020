2d901ed96cb8178fbd5076d814d8b57bdc1c425d
tombavarsky <tomba16062004@gmail.com>
2020-02-10 16:58:59 +0200

    resolved conflicts and fixed errors after continueing revert

--- src/ScoutingMain.elm
+++ src/ScoutingMain.elm
@@ -11,6 +11,7 @@ import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font exposing (center)
 import Element.Input exposing (button)
+import File.Download as Download
 import GetMatch
 import Html.Attributes exposing (style)
 import Result.Extra exposing (merge)
